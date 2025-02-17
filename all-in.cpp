c60d56777206a3d4a8e692f59dd6f8cebb4a21ae
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2025-02-17 13:17:54 +0100

    Adding spectra (conflicts)

--- all-in.cpp
+++ all-in.cpp
@@ -18,6 +18,7 @@
 
 #include <Ponca/Fitting>
 
+#include <Spectra/SymEigsSolver.h>
 
 int main(int argc, char** argv)
 {
@@ -96,5 +97,31 @@ int main(int argc, char** argv)
     << "Is projected at   " << fit.project(p).transpose() << std::endl;
   }
   
+
+  { //Specra
+    // We are going to calculate the eigenvalues of M
+    Eigen::MatrixXd A = Eigen::MatrixXd::Random(10, 10);
+    Eigen::MatrixXd M = A + A.transpose();
+
+    // Construct matrix operation object using the wrapper class DenseSymMatProd
+    DenseSymMatProd<double> op(M);
+
+    // Construct eigen solver object, requesting the largest three eigenvalues
+    SymEigsSolver<DenseSymMatProd<double>> eigs(op, 3, 6);
+
+    // Initialize and compute
+    eigs.init();
+    int nconv = eigs.compute(SortRule::LargestAlge);
+
+    // Retrieve results
+    Eigen::VectorXd evalues;
+    if (eigs.info() == CompInfo::Successful)
+      evalues = eigs.eigenvalues();
+
+    std::cout << "Eigenvalues found:\n"
+              << evalues << std::endl;
+              
+  }
+
   return 0;
 }
