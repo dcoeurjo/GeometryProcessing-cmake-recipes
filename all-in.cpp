d69cc985709c7be6141b4d43277e25ded6aea470
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2025-06-26 09:31:18 +0200

    upgrading CPM.cmake

--- all-in.cpp
+++ all-in.cpp
@@ -25,6 +25,13 @@
 
 #include <tsl/robin_map.h>
 
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point.hpp>
+#include <boost/geometry/geometries/box.hpp>
+#include <boost/geometry/index/rtree.hpp>
+#include <vector>
+#include <boost/foreach.hpp>
+
 int main(int argc, char** argv)
 {
   
@@ -128,7 +135,7 @@ int main(int argc, char** argv)
               
   }
 
-  {
+  { //tsl::robin_map
     tsl::robin_map<int, int> map = {{1, 1}, {2, 1}, {3, 1}};
     for (auto it = map.begin(); it != map.end(); ++it)
     {
@@ -159,6 +166,44 @@ int main(int argc, char** argv)
     
     std::cout << "Signed area = " << signed_area(tri)<<std::endl;
     
+  }
+
+
+  {//boost
+    namespace bg = boost::geometry;
+    namespace bgi = boost::geometry::index;
+    typedef bg::model::point<float, 2, bg::cs::cartesian> point;
+    typedef bg::model::box<point> box;
+    typedef std::pair<box, unsigned> value;
+    // create the rtree using default constructor
+    bgi::rtree<value, bgi::quadratic<16>> rtree;
+    // create some values
+    for (unsigned i = 0; i < 10; ++i)
+    {
+      // create a box
+      box b(point(i + 0.0f, i + 0.0f), point(i + 0.5f, i + 0.5f));
+      // insert new value
+      rtree.insert(std::make_pair(b, i));
+    }
+    // find values intersecting some area defined by a box
+    box query_box(point(0, 0), point(5, 5));
+    std::vector<value> result_s;
+    rtree.query(bgi::intersects(query_box), std::back_inserter(result_s));
+    // find 5 nearest values to a point
+    std::vector<value> result_n;
+    rtree.query(bgi::nearest(point(0, 0), 5), std::back_inserter(result_n));
+    // display results
+    std::cout << "spatial query box:" << std::endl;
+    std::cout << bg::wkt<box>(query_box) << std::endl;
+    std::cout << "spatial query result:" << std::endl;
+    BOOST_FOREACH (value const &v, result_s)
+      std::cout << bg::wkt<box>(v.first) << " - " << v.second << std::endl;
+
+    std::cout << "knn query point:" << std::endl;
+    std::cout << bg::wkt<point>(point(0, 0)) << std::endl;
+    std::cout << "knn query result:" << std::endl;
+    BOOST_FOREACH (value const &v, result_n)
+      std::cout << bg::wkt<box>(v.first) << " - " << v.second << std::endl;
   }
 
   return 0;
--- recipes/dgtal.cmake
+++ recipes/dgtal.cmake
@@ -3,6 +3,7 @@ if (TARGET DGtal)
 endif()
 
 include(CPM)
+include(boost)
 
 message(STATUS "Fetching DGtal")
 SET(BUILD_EXAMPLES OFF)
@@ -12,9 +13,12 @@ CPMAddPackage(
   NAME DGtal
   GITHUB_REPOSITORY "DGtal-team/DGtal"
   GIT_TAG 1.4.2.1
+  OPTIONS "-DBoost_DIR=${Boost_INCLUDE_DIRS}"
 )
 
 include("${DGtal_BINARY_DIR}/DGtalConfig.cmake")
 include_directories("${DGTAL_INCLUDE_DIRS}")
+message(STATUS "DGtal include dirs: ${DGTAL_INCLUDE_DIRS}")
+message(STATUS "Boost include dirs: ${Boost_DIRS}")
 
 ## Link targets to use: "DGtal ${DGTAL_LIBRARIES}"
