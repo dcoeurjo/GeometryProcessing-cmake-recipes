6cee93f291afabcc04e4ce190aa41d312f5f1d82
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2025-09-12 10:27:30 +0200

    Update dgtal.cmake

--- recipes/dgtal.cmake
+++ recipes/dgtal.cmake
@@ -13,9 +13,3 @@ CPMAddPackage(
   GITHUB_REPOSITORY "DGtal-team/DGtal"
   GIT_TAG 8978c75a29cb4d4a8c44252c0ebde4a3def53f9f
 )
-
-include("${DGtal_BINARY_DIR}/DGtalConfig.cmake")
-include_directories("${DGTAL_INCLUDE_DIRS}")
-message(STATUS "Boost include dirs: ${Boost_DIRS}")
-
-## Link targets to use: "DGtal ${DGTAL_LIBRARIES}"
